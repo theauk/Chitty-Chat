28c080ee0a8667b3e2f40b78d4a4a2fb1e463c6e
Thea <tkjeldsmark@gmail.com>
2021-10-28 16:10:03 +0200

    Updated server

--- Client/client.go
+++ Client/client.go
@@ -3,29 +3,54 @@ package main
 import (
 	//"context"
 	//"log"
+
+	"context"
 	"fmt"
-	"log"
-	"net"
-	"os"
-   
-    //første er init navnet
-	cc "chittychat/ChittyChat"
+	"sync"
 
-    "google.golang.org/grpc"
+	//første er init navnet
+	proto "chittychat/ChittyChat"
 )
 
-const (
-	defaultName = "chitty"
-)
+var client proto.BroadcastClient
+var wait *sync.WaitGroup
+
+func init() {
+	wait = &sync.WaitGroup{}
+}
+
+func connect(user *proto.Client) error {
+	var streamError error
+
+	stream, err := client.CreateStream(context.Background(), &proto.Connect{
+		User:   user,
+		Active: true,
+	})
+
+	if err != nil {
+		return fmt.Errorf("Failed connection")
+	}
+
+	wait.Add(1)
+	go func(str proto.Broadcast_CreateStreamClient) { // the streaming part of the client
+
+		defer wait.Done()
+
+		for {
+
+			msg, err := str.Recv() // wait for us to recieve a message from the server
+			if err != nil {
+				streamError = fmt.Errorf("Could not read message")
+				break
+			}
+
+			fmt.Printf(msg.Id, msg.Message) // log ?
+		}
+
+	}(stream)
+
+}
 
 func main() {
-	address := ":" + os.Args[1]
-	
-    conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
-	}
-	defer conn.Close()
 
-	client := cc.NewChittyClient(conn)
 }
--- Server/server.go
+++ Server/server.go
@@ -1,39 +1,93 @@
 package main
 
-import(
- "log"
-// "context"
- "net"
+import (
+	"context"
+	"log"
+	"sync"
 
-"google.golang.org/grpc"
+	// "context"
+	proto "chittychat/ChittyChat"
+	"net"
 
+	"google.golang.org/grpc"
 )
 
-const(
-    port = ":8000"
+const (
+	port = ":8000"
 )
 
-type server struct{
-    ChannelOut chan string
+type Connection struct {
+	stream proto.Broadcast_CreateStreamServer
+	id     string
+	active bool
+	error  chan error
 }
 
-func main(){
-    lis, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+type Server struct {
+	Connection []*Connection // collection of connections (pointing to connections)
+}
+
+func (s *Server) CreateStream(pcon *proto.Connect, stream proto.Broadcast_CreateStreamServer) error {
+	con := &Connection{ // ?
+		stream: stream,
+		id:     pcon.User.Id,
+		active: true,
+		error:  make(chan error),
 	}
-	s := grpc.NewServer()
-    if err := s.Serve(lis); err != nil {
-    log.Fatalf("failed to serve: %v", err)
-    }
+
+	s.Connection = append(s.Connection, con)
+	return <-con.error
 }
 
-/* 
-func (s *server) Join (ctx context.Context, in *pb.JoinRequest) (*pb.JoinConfirm, error){
+func (s *Server) BroadcastMessage(c context.Context, message *proto.Message) (*proto.Close, error) {
+	wait := sync.WaitGroup{} // waits for the go routines to finish
+	done := make(chan int)   // to know when all the go routines are finished
+
+	for _, c := range s.Connection {
+		wait.Add(1) // add new go routine to wait group
+
+		go func(message *proto.Message, c *Connection) {
+			defer wait.Done()
+
+			if c.active {
+				err := c.stream.Send(message) // send message back to the client that is attached connection
+				log.Println("Message being sent to: ", c.stream)
+
+				if err != nil {
+					log.Println("Could not send message")
+					c.active = false
+					c.error <- err
+				}
+			}
+
+		}(message, c)
+
+		go func() { // another go routine that runs and ensures that the waitgroup will wait for the other go routines
+			wait.Wait()
+			close(done)
+		}()
+
+		<-done // block the return statement until routines are done. Done needs to return something before we can return something
+		return &proto.Close{}, nil
+
+	}
+
 }
 
-func (s *server) Publish(){
-}
+func main() {
 
-func (s *server) Broadcast(){
-} */
+	var connections []*Connection  // Pointers to connections
+	server := &Server{connections} // Make server with connection array
+
+	serverGrpc := grpc.NewServer()              // Start server
+	listener, err := net.Listen("tcp", ":8080") // Listen port 8080
+
+	if err != nil {
+		log.Fatalf("Could not create the server")
+	}
+
+	log.Println("Started server at port 8080")
+
+	proto.RegisterBroadcastServer(serverGrpc, server)
+	serverGrpc.Serve(listener)
+}
